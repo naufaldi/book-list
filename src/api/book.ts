a2c674e90ba7a8fa0b2796f9ad9734e0da038c66
Naufaldi Rafif S <naufaldi.rafif@efishery.com>
2024-05-22 16:09:08 +0700

    fix: env variable, favicon

--- src/api/book.ts
+++ src/api/book.ts
@@ -1,15 +1,13 @@
 import { Book } from '@/interfaces/book';
 
-const API_URL = 'https://my-json-server.typicode.com/cutamar/mock/books';
-
 export const fetchBooks = async (): Promise<Book[]> => {
-  const response = await fetch(API_URL);
+  const response = await fetch(import.meta.env.VITE_API_URL);
   const data = await response.json();
   return data;
 };
 
 export const fetchBookId = async (id: string): Promise<Book> => {
-  const response = await fetch(`${API_URL}/${id}`);
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/${id}`);
   const data = await response.json();
   return data;
 };
